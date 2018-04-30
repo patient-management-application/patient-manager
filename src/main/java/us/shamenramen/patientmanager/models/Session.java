1dda57803a86b70fc571b4962e9e413c76af95d8
armandosegura <segura_arm@yahoo.com>
2018-04-30 15:02:26 -0500

    added Session model, first commit

---
+++ src/main/java/us/shamenramen/patientmanager/models/Session.java
@@ -0,0 +1,121 @@
+package us.shamenramen.patientmanager.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="Session")
+public class Session {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column(name="pat_id")
+    private long patientId;
+
+    @Column(name="doc_id")
+    private long doctorId;
+
+    @Column
+    private String notes;
+
+    @Column
+    private String prescriptions;
+
+    @Column
+    private String procedures;
+
+    @Column(name="time_start")
+    private String timeStart;
+
+    @Column(name="time_end")
+    private String timeEnd;
+
+    public Session() {
+    }
+
+    public Session(long patientId, long doctorId, String notes, String prescriptions, String procedures, String timeStart, String timeEnd) {
+        this.patientId = patientId;
+        this.doctorId = doctorId;
+        this.notes = notes;
+        this.prescriptions = prescriptions;
+        this.procedures = procedures;
+        this.timeStart = timeStart;
+        this.timeEnd = timeEnd;
+    }
+
+    public Session(long id ,long patientId, long doctorId, String notes, String prescriptions, String procedures, String timeStart, String timeEnd) {
+        this.id = id;
+        this.patientId = patientId;
+        this.doctorId = doctorId;
+        this.notes = notes;
+        this.prescriptions = prescriptions;
+        this.procedures = procedures;
+        this.timeStart = timeStart;
+        this.timeEnd = timeEnd;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(long patientId) {
+        this.patientId = patientId;
+    }
+
+    public long getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctorId(long doctorId) {
+        this.doctorId = doctorId;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public String getPrescriptions() {
+        return prescriptions;
+    }
+
+    public void setPrescriptions(String prescriptions) {
+        this.prescriptions = prescriptions;
+    }
+
+    public String getProcedures() {
+        return procedures;
+    }
+
+    public void setProcedures(String procedures) {
+        this.procedures = procedures;
+    }
+
+    public String getTimeStart() {
+        return timeStart;
+    }
+
+    public void setTimeStart(String timeStart) {
+        this.timeStart = timeStart;
+    }
+
+    public String getTimeEnd() {
+        return timeEnd;
+    }
+
+    public void setTimeEnd(String timeEnd) {
+        this.timeEnd = timeEnd;
+    }
+}
