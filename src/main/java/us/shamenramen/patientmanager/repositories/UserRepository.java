e510997439cee613442c6f699775f9cdffe766b4
benjaminhodge <benjamin.andrew.hodge@gmail.com>
2018-05-01 09:12:36 -0500

    fixed user repository and added annotations for repos

--- src/main/java/us/shamenramen/patientmanager/repositories/UserRepository.java
+++ src/main/java/us/shamenramen/patientmanager/repositories/UserRepository.java
@@ -1,8 +1,12 @@
 package us.shamenramen.patientmanager.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import us.shamenramen.patientmanager.models.User;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
-    public User findByUsername(String username);
+//    No username is defined on the user object.
+    User findByEmail(String email);
+    User findById(long id);
 }
