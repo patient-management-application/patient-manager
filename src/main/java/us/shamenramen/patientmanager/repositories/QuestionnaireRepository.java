51e0ea24fe15a2e51dbf1fd6413e013a49e59154
armandosegura <segura_arm@yahoo.com>
2018-05-01 11:09:17 -0500

    added more respositories, controllers, working on relationships, building views to work

---
+++ src/main/java/us/shamenramen/patientmanager/repositories/QuestionnaireRepository.java
@@ -0,0 +1,10 @@
+package us.shamenramen.patientmanager.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import us.shamenramen.patientmanager.models.Questionnaire;
+
+@Repository
+public interface QuestionnaireRepository extends CrudRepository<Questionnaire, Long>{
+
+}
