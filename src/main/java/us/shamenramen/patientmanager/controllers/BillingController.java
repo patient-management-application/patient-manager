f7248b24aa7c4c2626b0dceecbea0f997a9f7121
armandosegura <segura_arm@yahoo.com>
2018-05-08 14:16:16 -0500

    almost have the logic for controllers, need make createInvoice and sendInvoice

--- src/main/java/us/shamenramen/patientmanager/controllers/BillingController.java
+++ src/main/java/us/shamenramen/patientmanager/controllers/BillingController.java
@@ -1,15 +1,13 @@
 package us.shamenramen.patientmanager.controllers;
 
-import com.stripe.exception.StripeException;
-import com.stripe.model.Charge;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import us.shamenramen.patientmanager.models.Bill;
+import us.shamenramen.patientmanager.models.ChargeRequest;
 import us.shamenramen.patientmanager.models.User;
 import us.shamenramen.patientmanager.repositories.UserRepository;
 
@@ -19,8 +17,6 @@ import java.util.Currency;
 public class BillingController {
 
     private UserRepository userDao;
-//    @Autowired
-//    private StripeService paymentsService;
 
     public BillingController(UserRepository userDao) {
         this.userDao = userDao;
@@ -39,22 +35,18 @@ public class BillingController {
 
 
 
-//    @PostMapping("/charge")
-//    public String charge(ChargeRequest chargeRequest, Model model)
-//            throws StripeException {
-//        chargeRequest.setDescription("Example charge");
-//        chargeRequest.setCurrency(Currency.EUR);
-//        Charge charge = paymentsService.charge(chargeRequest);
-//        model.addAttribute("id", charge.getId());
-//        model.addAttribute("status", charge.getStatus());
-//        model.addAttribute("chargeId", charge.getId());
-//        model.addAttribute("balance_transaction", charge.getBalanceTransaction());
-//        return "result";
-//    }
 
-    @ExceptionHandler(StripeException.class)
-    public String handleError(Model model, StripeException ex) {
-        model.addAttribute("error", ex.getMessage());
-        return "result";
+
+
+
+    @Value("${sk_test_JDCutAUR2DvZFbSiCtGvjC48}")
+    private String stripePublicKey;
+
+    @RequestMapping("/checkout")
+    public String checkout(Model model) {
+        model.addAttribute("amount", 50 * 100); // in cents
+        model.addAttribute("stripePublicKey", stripePublicKey);
+        model.addAttribute("currency", ChargeRequest.Currency.EUR);
+        return "checkout";
     }
 }
